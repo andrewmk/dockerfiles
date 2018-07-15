8a61632fad2873965e5b5f7aed028ce0a82af6d5
Alex Haydock <alex@alexhaydock.co.uk>
2018-07-15 11:57:39 +0100

    Add UID/GID switcher to Transmission

---
+++ transmission-daemon/x86_64/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Set config location
+export TRANSMISSION_HOME='/transmission/config'
+
+# If we specified a UID/GID with docker run or compose, change the Transmission user/group to it
+[[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o transmission
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o transmission
+
+# Start Transmission
+sudo -u transmission "/usr/bin/transmission-daemon && sleep 999d"
