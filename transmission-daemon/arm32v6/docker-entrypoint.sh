d0aadd38657b7a7cfcf36a3ba3078cef64b16c7a
Alex Haydock <alex@alexhaydock.co.uk>
2018-07-15 23:09:40 +0100

    Transmission permissions fix

--- transmission-daemon/arm32v6/docker-entrypoint.sh
+++ transmission-daemon/arm32v6/docker-entrypoint.sh
@@ -7,5 +7,8 @@ export TRANSMISSION_HOME='/transmission/config'
 [[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o transmission
 [[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o transmission
 
+# Make sure we can actually read the config
+chown -R "$USERID":"$GROUPID" "$TRANSMISSION_HOME"
+
 # Start Transmission
 sudo --preserve-env -u transmission "/usr/bin/transmission-daemon" && sleep 999d
